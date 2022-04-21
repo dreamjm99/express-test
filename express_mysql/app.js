7242a5b18416621677523ef3f27535c982d38c9b
jaemo <dreamjm99@gmail.com>
2022-04-21 17:08:43 +0900

    express test1

--- express_mysql/app.js
+++ express_mysql/app.js
@@ -5,6 +5,12 @@ const ejs = require('ejs')
 const app = express()
 const port = 3000
 
+
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine' ,'ejs');
+
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
@@ -14,42 +20,63 @@ const con = mysql.createConnection({
     database: 'express_db'
 });
 
-app.listen(port , () => console.log(`Example app listening on port ${port}!`))
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.set('view engine' ,'ejs');
-
-//insert form
-// app.get('/', (req,res) => 
-//     res.sendFile(path.join(__dirname, "html/form.html")))
-
-// app.post('/', (req, res) =>{
-//     const sql = "INSERT INTO users SET ?"
-
-//     con.query(sql,req.body,function(err,result,fields){
-//         if (err) throw err;
-//         console.log(result)
-//         res.send('등록이 완료 되었습니다');
-//     });
-// });
 
 
-
-
-//list_form
 con.connect(function(err) {
     if (err) throw err;
     console.log('Connected');
 });
 
-app.get('/', (req, res) => { //select
-    const sql = "select * from users"
+app.get('/', (req, res) => { //select 리스트조회
+    const sql = "select * from users";
     con.query(sql , function (err, result, fields){
         if (err) throw err;
         res.render('index', {users : result});
     });
 });
 
+app.post('/', (req,res) => {
+    const sql = "INSERT INTO users set ?"
+    con.query(sql,req.body,function(err,result,fields){
+        if (err) throw err;
+        console.log(result);
+        res.redirect('/');
+    });
+});
+
+app.get('/create', (req,res) =>
+    res.sendFile(path.join(__dirname, 'html/form.html')))
+
+
+
+app.get('/delete/:id',(req,res) =>{//삭제
+    const sql = "DELETE FROM users WHERE id = ?"; 
+    con.query(sql,[req.params.id],function(err,result,fields){
+        if (err) throw err;
+        console.log(result);
+        res.redirect('/')
+    });
+});
+
+app.post('/update/:id',(req,res)=>{//회원정보 수정
+    const sql = "UPDATE users SET ? WHERE id = " + req.params.id;
+    con.query(sql,req.body,function (err,result,fields){
+        if (err) throw err;
+        console.log(result);
+        res.redirect('/');
+    });
+});
+
+app.get('/edit/:id', (req, res) => { //수정할 회원정보 조회
+    const sql = "SELECT * FROM users WHERE id=?";
+    con.query(sql ,[req.params.id],function (err, result, fields){
+        if (err) throw err;
+        res.render('edit', {user : result});
+    });
+});
+
+
+app.listen(port , () => console.log(`Example app listening on port ${port}!`))//서버 실행
 
 
 
